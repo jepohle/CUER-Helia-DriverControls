b3efa3614e23f6044d7cd9479e07c989d1e46534
jepohle <jep78@cam.ac.uk>
2023-01-29 23:38:44 +0000

    added further definitions, functions and function prototypes

--- IOFunctions.cpp
+++ IOFunctions.cpp
@@ -11,18 +11,30 @@ void IOinit(void){
     DigitalIn rear_fog(REAR_FOG_PIN);
     DigitalIn regen_en(REGEN_EN_PIN);
     DigitalIn solar_en(SOLAR_PIN);
-    
+    DigitalIn horn(HORN_PIN);
+
     AnalogIn throttle(THROTTLE_PIN);
     AnalogIn brake(BRAKE_PIN);
 
     InterruptIn cruise_en(CRUISE_EN_PIN);
     InterruptIn ignition(IGNITION_PIN);
-    InterruptIn horn(HORN_PIN);
     InterruptIn gear(REVERSE_PIN);
-    // InterruptIn mode(DRIVE)
+    // InterruptIn mode(DRIVE_)
 
 }
 
 void ReadIO(void){
-    
+if(brake >= BRAKE_POT_MIN+0.3){
+    brake_state = true;
+}
+rightIndicatorActive = right_ind | hazards;
+leftIndicatorActive = left_ind | hazards;
+hornActive = horn;
+if(cruise_dec){
+    cruiseSetpoint -= 1.0;
+}
+if(cruise_inc){
+    cruiseSetpoint += 1.0;
+}
+solar_state = solar_en;
 }
--- main.cpp
+++ main.cpp
@@ -2,30 +2,114 @@
 #include "CAN.h"
 #include "main.h"
 #include "IOFunctions.h"
+#include <cstdint>
 
 
 // Define constants and addresses
 #define WHEEL_RADIUS_M 0.275
 #define V_MAX_KMH 100.0
 #define I_BUS_MAX 122.0
-// #define THROTTLE_POT_MIN 0.11             MEASURE DEFINITIONS!
-// #define THROTTLE_POT_MAX 0.22
-// #define BRAKE_POT_MIN
-// #define BRAKE_POT MAX
+#define THROTTLE_POT_MIN 0.11         //    MEASURE DEFINITIONS!
+#define THROTTLE_POT_MAX 0.22
+#define BRAKE_POT_MIN 0.11
+#define BRAKE_POT_MAX 0.22
 
 
 // Define variables
 bool ignition_state = false;
 bool cruise_state = false;
-bool brake_state =false;
- 
+bool brake_state = false;
+bool solar_state = false;
+bool rightIndicatorActive = false;
+bool leftIndicatorActive = false;
+bool hornActive = false;
+
+int driveMode = 0;    //Modes correspond to 0: regular torque controlled full throw driving
+                  //                    1: low power single motor drive, motor allocated randomly, unless error in one motor controller
+                  //                    2: no regen, just coast to stop when throttle lifted
+                  //                    3: 
+
+float speedSetpoint;
+float motorCurrentSetpoint;
+float maxBusCurrentSetpoint;
+float currentSpeed;
+float currentMotorCurrent;
+float currentBusCurrent;
+float cruiseSetpoint;
+
+// Define useful flags
+bool IOStateChangeFlag;
+
+
+// define empty CAN Messages
+CANMessage TxMessage;
+CANMessage RxMessage;
+
+// Function prototypes              IMPLEMENT THESE!
+void calculateThrottle(void);
+void CANIT_Callback(void);
+void sendDriveCommand(float, float, float, int);
+void ignitionIT_Callback(void);
+void gearIT_Callback(void);
+void cruiseIT_Callback(void);
+void modeIT_Callback(void);
 
 
 // main() runs in its own thread in the OS
 int main()
 {
+    // setup CAN interface:
+    CAN CAN1(CAN_RD_PIN, CAN_TD_PIN, 500000);
+    CAN1.attach(&CANIT_Callback);
+
+    // initialise IOs
+    IOinit();
+    ignition.rise(&ignitionIT_Callback);
+    gear.rise(&gearIT_Callback);
+    cruise_en.rise(&cruiseIT_Callback);
+    mode.rise(&modeIT_Callback);
+
+
+
+
+
     while (true) {
 
     }
 }
 
+void calculateThrottle(void){
+
+}
+
+void CANIT_Callback(void){
+
+}
+
+void sendDriveCommand(float, float, float, int){
+
+}
+
+void ignitionIT_Callback(void){
+
+}
+
+void gearIT_Callback(void){
+
+}
+
+void cruiseIT_Callback(void){
+
+}
+
+void modeIT_Callback(void){
+
+}
+
+void Error_Handler(void){
+    // Enter infinite loop of sending 
+    TxMessage = CANMessage(CAN_DRIVER_CONTROLS_BASE, 0b10000000, 1);
+    while(1){
+        CAN1.write(TxMessage);
+    }
+}
